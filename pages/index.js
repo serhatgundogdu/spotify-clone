8d57f8160d6b258ea25e619ff8b6378313d3118a
serhatgundogdu <serhatgnn1@hotmail.com>
2021-04-24 02:47:56 +0300

    v0.0.1

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import Image from 'next/image'
+import Link from 'next/link'
+
 
 export default function Home() {
   return (
@@ -9,7 +12,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.container}>
-        asd
+        <div className={styles.sidebar}>
+          <h2 className={styles.logo}>Spotify</h2>
+          
+          <ul className={styles.menus}>
+            <li className={styles.active}>
+              <Link href="/">Anasayfa</Link>
+            </li>
+            <li>
+              <Link  href="/search">Ara</Link>
+            </li>
+            <li>
+              <Link href="/collection/playlists">Kitaplığın</Link>
+            </li>
+          </ul>
+        </div>
       </div>
       <div className={styles.bar}>
         asd

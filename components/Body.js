92512b92fd107a7b90abd2f6de69015e6c6f0351
serhatgundogdu <serhatgnn1@hotmail.com>
2021-04-24 17:16:30 +0300

    lul

--- components/Body.js
+++ components/Body.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {useState} from 'react'
+import Sidebar from './Sidebar'
 
 
 export default function Body(props) {
@@ -18,28 +19,9 @@ export default function Body(props) {
 
     return (
         <>
+            
             <div className={styles.container}>
-                <div className={styles.sidebar}>
-                    <h2 className={styles.logo}>Şupotifay</h2>
-                    
-                    <ul className={styles.menus}>
-                        <li className={props.activepage == 'home' && styles.active}>
-                        <Link href="/">
-                            <a><FontAwesomeIcon icon="home" /> Anasayfa</a>       
-                        </Link>
-                        </li>
-                        <li className={props.activepage == 'search' && styles.active}>
-                        <Link  href="/search">
-                            <a><FontAwesomeIcon icon="search" /> Ara</a>   
-                        </Link>
-                        </li>
-                        <li className={props.activepage == 'bookmark' && styles.active}>
-                        <Link href="/">
-                            <a><FontAwesomeIcon icon="bookmark"/> Kitaplığın</a>   
-                        </Link>
-                        </li>
-                    </ul>
-                </div>
+                <Sidebar {...props}/>
                 <div className={styles.page}>
                     <div className={styles.header}>
                         <div>
---
+++ components/Sidebar.js
@@ -0,0 +1,66 @@
+import styles from '../styles/Home.module.scss'
+import MousePosition from '../hooks/MousePosition'
+import Image from 'next/image'
+import Link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {useState, useEffect} from 'react'
+
+
+export default function Sidebar(props) {
+    const[width, SetWidth] = useState(241);
+    const [isMouseDown, setisMouseDown] = useState(false);
+    const { x } = MousePosition();
+
+    useEffect(() => {
+      if (!isMouseDown) return false;
+  
+      const handleMove = () => {
+        if(x > 200 && x < 400){
+          SetWidth(x);
+        }
+      };
+  
+      const handleUp = () => {
+        setisMouseDown(false);
+      };
+  
+      document.addEventListener("mousemove", handleMove);
+      document.addEventListener("mouseup", handleUp);
+      return () => {
+        document.removeEventListener("mousemove", handleMove);
+        document.removeEventListener("mouseup", handleUp);
+      };
+    });
+    return (
+        <div className={styles.sidebar} style={{width: `${width}px`}}>
+            <div>
+                <h2 className={styles.logo}>Şupotifay</h2>      
+                <ul className={styles.menus}>
+                    <li className={props.activepage == 'home' && styles.active}>
+                        <Link href="/">
+                            <a><FontAwesomeIcon icon="home" /> Anasayfa</a>       
+                        </Link>
+                    </li>
+                    <li className={props.activepage == 'search' && styles.active}>
+                        <Link  href="/search">
+                            <a><FontAwesomeIcon icon="search" /> Ara</a>   
+                        </Link>
+                    </li>
+                    <li className={props.activepage == 'bookmark' && styles.active}>
+                        <Link href="/">
+                            <a><FontAwesomeIcon icon="bookmark"/> Kitaplığın</a>   
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div
+                className={`${styles.changeWidth} ${isMouseDown ? styles.ActiveChange : ''}`}
+                onMouseDown={() => {
+                setisMouseDown(true);
+                }}
+            >
+            </div>
+        </div>
+    )
+
+}
